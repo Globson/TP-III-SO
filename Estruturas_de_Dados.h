4f69dbe526ce3d99410ef355b32e2d5f27d1d908
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2020-11-23 16:45:57 -0300

    Add files via upload

---
+++ Estruturas_de_Dados.h
@@ -0,0 +1,34 @@
+#ifndef ESTRUTURAD_DE_DADOS_H
+#define ESTRUTURAD_DE_DADOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#define MAXTAM 100
+#define BUFFER 256
+
+typedef char Instrucao[20]; //Armazena uma instrução
+
+typedef char Memoria[1000];
+
+typedef struct Programa{
+  Instrucao instrucoes[20]; //Armazena instruções de um programa
+  int Tam; //Tamanho do programa;
+  int Frente,Tras; //Fila de programas
+}Programa;
+
+typedef struct EstadoProcesso{
+  int Quant_Inteiros;
+  int Alocado_V_inteiros;
+  int *Inteiro;
+  int Cont;
+  int Tam;
+  Instrucao Programa[20];
+}EstadoProcesso;
+
+#endif //ESTRUTURAD_DE_DADOS_H
