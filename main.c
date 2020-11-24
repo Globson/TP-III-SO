dfad1971f22cdc93aa91bffa4f07200298878d0b
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2020-11-24 18:59:17 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,29 +4,17 @@
 #include "Headers/Estruturas_de_Dados.h"
 #include <time.h>
 
-void iniciamapa(int mapadebits[]){
-    for(int i=0;i < MAXMEM;i++){
-        mapadebits[i] = 0;
-    }
-}
 
 void iniciamemoria(int memoria[]){
     for(int i=0;i < MAXMEM;i++){
         memoria[i] = 0;
+        mapadebits[i] = 0;
     }
 }
 
-/*
-void Test(char memoria[]){
-    for(int i=0;i < MAXMEM;i++){
-        printf("%c\n",memoria[i]);
-    }
-}
-*/
 
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
-  iniciamapa(mapadebits);
   iniciamemoria(memoria);
   //Test(memoria);
   Executar_P_Controle();
