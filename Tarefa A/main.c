8032099bfb3ddb5a167de182ef3f12064052f061
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2020-11-25 22:27:59 -0300

    Add files via upload

--- Tarefa A/main.c
+++ Tarefa A/main.c
@@ -5,17 +5,28 @@
 #include <time.h>
 
 
-void iniciamemoria(int memoria[]){
+void iniciamemoria(){
     for(int i=0;i < MAXMEM;i++){
-        memoria[i] = 0;
-        mapadebits[i] = 0;
+        firstfit.memoria[i] = 0;
+        firstfit.mapadebits[i] = 0;
+        nextfit.memoria[i] = 0;
+        nextfit.mapadebits[i] = 0;
     }
+    firstfit.totalalocados = 0;
+    firstfit.nospercorridos = 0;
+    firstfit.totalfragmentos = 0;
+    firstfit.erroemalocar = 0;
+    nextfit.totalalocados = 0;
+    nextfit.nospercorridos = 0;
+    nextfit.totalfragmentos = 0;
+    nextfit.erroemalocar = 0;
+    nextfit.ultimaalocacao = 0;
 }
 
 
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
-  iniciamemoria(memoria);
+  iniciamemoria();
   //Test(memoria);
   Executar_P_Controle();
   printf("\n\tExecutou TUDO!");
